51f8a787f6d15df8db60017c7048b1607a5f6b97
Bill <wkernan@gmail.com>
2016-11-28 20:55:17 -0600

    tweets component

---
+++ components/Tweets.react.js
@@ -0,0 +1,16 @@
+var React = require('react');
+var Tweet = require('./Tweet.react.js');
+
+module.exports = Tweets = React.createClass({
+	render: function() {
+		var content = this.props.tweets.map(function(tweet) {
+			return(
+				<Tweet key={tweet.twid} tweet={tweet} />
+			)
+		});
+
+		return(
+			<ul className="tweets">{content}</ul>
+		)
+	}
+});
